efe0a8ff8ef39c8573fd9e7560552ae68109007c
Ruby Dong <ruby.dong@stonybrook.edu>
2017-03-23 09:58:33 -0400

    updated looks

--- twitter.js
+++ twitter.js
@@ -6,8 +6,8 @@ var bodyParser = require("body-parser");
 var nodemailer = require("nodemailer");
 var MongoClient = require("mongodb").MongoClient;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname));
 app.set("trust proxy", 1); //Trust first proxy
 app.use(cookieSession({
@@ -15,7 +15,7 @@ app.use(cookieSession({
     keys: [(Math.random() + 1).toString(36).substring(7)]
 }));
 
-//var emailKey = ""; //For email verification
+var emailKey = ""; //For email verification
 
 MongoClient.connect("mongodb://localhost:27017/twitter", function (error, database) {
     
@@ -56,7 +56,8 @@ function sendEmail(email, key) {
 }
          
 app.post("/adduser", function (request, response) {
-    console.log("IN ADDUSER POST");
+	console.log("IN ADDUSER POST");
+    
     var username = request.body.username;
     var password = request.body.password;
     var email = request.body.email;    
@@ -67,18 +68,16 @@ app.post("/adduser", function (request, response) {
         db.collection("users").findOne( {"username": username}, { "conversations": 1 }, function (error, document) {  
             if (document) {
                 //user exists 
-                response.json({status: "ERROR", "error": "USERNAME ALREADY EXISTS"});
-                
+                response.json({"status": "ERROR", "Error": "USERNAME ALREADY EXISTS"});
             } else {
                 //user does not exist check emails now
                 db.collection("users").findOne( {"email": email}, { "conversations": 1 }, function (error, document) {  
                     if (document) {
                         //email exists
                         exists = true;
-                        response.json({status: "ERROR", "error": "EMAIL ALREADY EXISTS"});
+                        response.json({"status": "ERROR", "Error": "EMAIL ALREADY EXISTS"});
                     } else {
-                        //username and email does not exist
-                        //DO NOT SEND EMAIL FOR NOW!
+                        //username and email does not exist do this
                         //sendEmail (email, emailkey);
                         var document = {
                             "username": request.body.username,
@@ -88,15 +87,13 @@ app.post("/adduser", function (request, response) {
                             "tweets": []
                         }; 
                         db.collection("users").insert(document, {w: 1}, function(error, result) {});
-                        
-                        response.json({status: "OK"});
-                        
+                        response.json({"status": "OK"});
                     }
                 });
             }
         });
     } else {
-        response.json({status: "ERROR", "error": "PLEASE FILL IN ALL FIELDS"});
+        response.json({"status": "ERROR", "Error": "PLEASE FILL IN ALL FIELDS"});
     }
 });
         
@@ -105,39 +102,43 @@ app.get("/login", function (request, response) {
 });
 
 app.post("/login", function (request, response) {
-    console.log("IN LOGIN POST");
+	console.log("IN LOGIN POST");
     var username = request.body.username;
     var password = request.body.password;
     
     if (username && password) {
-        console.log("user: %s pass: %s", username, password);
+		console.log("user: %s pass: %s", username, password);
         var username = request.body.username;
         db.collection("users").findOne({ "username": username, "password": request.body.password, "verified": "yes" }, { "name": 1 }, function (error, document) {
             if (document) {
                 //sets the cookie 
                 request.session.username = username;
-                response.json({status: "OK"});
+                response.json({"status": "OK"});
             } else {
-                response.json({status:"ERROR", "error": "INVALID LOGIN"});
+                response.json({"status":"ERROR", "Error": "INVALID LOGIN"});
             }
         });
        
     } else {
-        response.json({status: "ERROR", "error": "PLEASE FILL IN ALL FIELDS"});
+        response.json({"status": "ERROR", "Error": "PLEASE FILL IN ALL FIELDS"});
     }
     
 });
 
-app.get("/logout", function (request, response) {            
-    response.sendFile(path.join(__dirname + "/logout.html"));
+app.get("/logout", function(request, response) {
+    console.log("IN LOGOUT POST");
+    request.session = null;
+    response.redirect('/login');
 });
+
 app.post("/logout", function (request, response) {
 //    if (request.session.isNew) {
-//        response.json({status: "ERROR", "error": "ALREADY LOGGED OUT"});
+//        response.json({status: "ERROR", "Error": "ALREADY LOGGED OUT"});
 //    } else {
-        console.log("IN LOGOUT POST");	
+        console.log("IN LOGOUT POST");
         request.session = null;
-        response.json({ status: "OK" });
+        response.json({ "status": "OK" });
+        response.redirect('/login');
 //    }
 });
 
@@ -146,25 +147,24 @@ app.get("/verify", function (request, response) {
 });
 
 app.post("/verify", function (request, response) {
-    console.log("IN VERIFY POST");
+	console.log("IN VERIFY POST");
     var email = request.body.email;
     var key = request.body.key;
 
     if (email && key) {
-        //if ( key == emailkey || key == "abracadabra") {
-        if (key == "abracadabra") {
-            console.log("IN HERE IN VERIFY");
-            response.json({status: "OK"});
+        if ( key == emailkey || key == "abracadabra") {
+			console.log("IN HERE IN VERIFY");
+            response.json({"status": "OK"});
             db.collection("users").update(
                 { "email": email }, 
                 { $set: { "verified": "yes" } } 
             );
         }
         else { 
-            response.json({status: "ERROR", "error": "INVALID KEY PLEASE TRY AGAIN"});
+            response.json({"status": "ERROR", "Error": "INVALID KEY PLEASE TRY AGAIN"});
         }
     } else {
-        response.json({status: "ERROR", "error": "PLEASE FILL IN ALL FIELDS"});
+        response.json({"status": "ERROR", "Error": "PLEASE FILL IN ALL FIELDS"});
     }
 });
 
@@ -174,6 +174,7 @@ app.get("/additem", function (request, response) {
 
 app.post("/additem", function (request, response) {
     console.log("IN ADDITEM POST");
+    
     var content = request.body.content;
     //if not logged in error
     var timestamp = new Date().getTime();
@@ -198,56 +199,57 @@ app.post("/additem", function (request, response) {
             },
             function (error, result) {
                 if (error) {
-                    response.json({ status: "ERROR" });
+                    response.json({ "status": "ERROR" });
                 } else {
+                    var document = {
+                        "id": id,   
+                        "username": request.session.username,
+                        "content": content,
+                        "timestamp": timestamp
+                    };
+                    
+                    db.collection("tweets").insert(document, {w: 1}, function(error, result) {if(error){console.log(error);}});
                     console.log("success");
+                    
                     response.json ({
                         status: "OK",
-                        id: id,
+                        item: document
                     });
                 }
             }
         );
     } else {
         response.json (
-            {status: "ERROR", "error": "USER IS NOT LOGGED IN"}
+            {status: "error", error: "USER IS NOT LOGGED IN"}
         );
     }
-    console.log("EXITED ADDITEM");
+	console.log("EXITED ADDITEM");
 });
-
 
 app.get("/item/:id", function (request, response) {
     var id = request.params.id;
     console.log("param id is.." + id);
     if (!request.session.isNew) {
-        db.collection("users").findOne( {"username": request.session.username}, { "tweets": 1 }, function (error, document) {
+		console.log("id is ", id);
+        db.collection("tweets").findOne( { "id": parseInt(request.params.id) },function (error, document) {
+			if (error) console.log(error);
+			console.log(document);
             if (document) {
-                var tweets = document.tweets;
-                
-                var found = false;
-                for (var i = 0; i < tweets.length; i++) {
-                    if (tweets[i].id == id) {
-                        found = true;
-                        response.json({
-                            status: "OK", 
-                            item: {
-                                "id": tweets[i].id, 
-                                "username": tweets[i].username,
-                                "content": tweets[i].content,
-                                "timestamp": tweets[i].timestamp
-                            }
-                        });
-                        break;
+                response.json({
+                    status: "OK",
+                    item: {
+                        id: document.id,
+                        username: document.username,
+                        content: document.content,
+                        timestamp: document.timestamp
                     }
-                }
-                if (!found) {
-                    response.json({status: "ERROR", "error": "THIS IS AN INVALID ID"});
-                }
+                });
+            } else {
+                response.json( { status: "error", error: "THIS IS AN INVALID ID" });
             }
         });
     } else {
-        response.json({status: "ERROR", "error": "USER IS NOT LOGGED IN"});
+        response.json({status: "error", error: "USER IS NOT LOGGED IN"});
     }
 });
 
@@ -256,62 +258,65 @@ app.get("/search", function(request, response) {
 });
 
 app.post("/search", function(request, response) {
-    console.log("IN SEARCH POST");		    
-	console.log(request);
+    console.log("IN SEARCH POST");
+    
     var timestamp = request.body.timestamp;
     /* optional */
     var limit = request.body.limit;
-    var currentLimit = 25;
-    //search through database for less than this time
-    //check if timestamp is empty
-    console.log("TIME STAMP: ", timestamp);		
-	if (request.body.timestamp) {		
-		console.log("timestamp exists");		
-		timestamp = timestamp * 1000;		
-	} else {		
-		timestamp = new Date().getTime();		
-		console.log("default timestamp ", timestamp);		
+    var currentLimit = 0;
+	limit = 25;
+	if (request.body.limit) {
+		limit = parseInt(request.body.limit);
 	}
+    console.log("limit is... " + typeof(limit));
     
+	if (request.body.timestamp) {
+		console.log(" timestamp exists");
+		timestamp = timestamp * 1000;
+	} else {
+		timestamp = new Date().getTime();
+		//console.log("default timestamp ", timestamp);
+	}
     if (timestamp) {
         if (!request.session.isnew && request.session.username != null) {
-             db.collection("users").findOne( {"username": request.session.username}, { "tweets": 1 }, function (error, document) {
-                if (document) {
-                    var tweets = document.tweets;
-                    var found = false;
-                    var items = new Array();
-                    console.log(tweets.length);
-                    for (var i = 0; i < tweets.length; i++) {
-                        console.log("curr ts: ", tweets[i].timestamp, "real ts: ", timestamp);
-                        if (tweets[i].timestamp <= timestamp) {
-                            console.log("am i here");	
-                            if (limit != "" && currentLimit >= limit){
-                                break;
-                            }
-                            currentLimit++;
-                            found = true;
-                            items.push(tweets[i]);
-                        }
-                    }
-                    console.log(items.length); 
-                    response.json({status:"OK", "items": items});
-                    
-                }
-            });
+			
+            var items = new Array();
+			db.collection("tweets").find({timestamp: {$lte: parseInt(timestamp)}}).limit(limit).each(function(err,val) {
+				if (val) {
+                    console.log("in here");
+                    if (currentLimit < limit) {
 
+                        if (val.username == request.session.username) {
+                                console.log("IN HERE BOYS");
+                        }
+                        console.log("pushing");
+                        items.push(val);
+                        console.log("val is... " + val);
+                        items.push({id:val.tweet_id,username:val.username,content:val.content,timestamp:val.timestamp});
+                        currentLimit++;
+                    }		
+				} else {
+				console.log("current limit... " + currentLimit);	
+
+				///console.log(JSON.stringify(items));
+				response.json({status:"OK", items:items});
+//1489624645603
+				}
+			});
+            console.log("items are..." + items);
         } else {
-            response.json({status: "ERROR", "error": "USER IS NOT LOGGED"});
+            response.json({status: "ERROR", "Error": "USER IS NOT LOGGED"});
         }
     } else {
-        response.json({status: "ERROR", "error": "TIMESTAMP IS EMPTY"});
+        response.json({status: "ERROR", "Error": "TIMESTAMP IS EMPTY"});
     }
 });
         
 app.listen(1337);
-app.listen(1338);		
-app.listen(1339);		
-app.listen(1340);		
-app.listen(1341);		
-app.listen(1342);		
+app.listen(1338);
+app.listen(1339);
+app.listen(1340);
+app.listen(1341);
+app.listen(1342);
 app.listen(1343);
 console.log("Server started");
