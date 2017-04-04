c7d99960ae4e985248f4eec8dd2f5f8cd2686267
jackzheng2496 <jackzheng2496@gmail.com>
2017-04-04 18:02:42 -0400

    refactoring search

--- twitter.js
+++ twitter.js
@@ -482,14 +482,14 @@ app.post("/search", function(req, res) {
 
                         for (var i = 0; i < val.length; i++) {
                             if (limitCounter < limit) {
-                                tweetsArr.push({
-                                    item: {
+                                tweetsArr.push(
+                                    "item" : {
                                      id: val[i].id,
                                      username: val[i].username,
                                      content: val[i].content,
                                      timestamp: val[i].timestamp
                                     }
-                                });
+                                );
                                 limitCounter++;
                             } else {
                                 break;
