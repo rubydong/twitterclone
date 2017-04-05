eb7d56841673fd5d3e05145857f374cb32d66b9c
jackzheng2496 <jackzheng2496@gmail.com>
2017-04-05 18:20:32 -0400

    get id

--- twitter.js
+++ twitter.js
@@ -402,7 +402,7 @@ function checkConditions(tweets, query, timestamp) {
 
 app.post("/search", function(req, res) {
 	console.log("IN SEARCH POST");
-    console.log(req.limit);
+    console.log(req.body.limit);
 
 	var timestamp = new Date().getTime();
 	var limit = 25;
