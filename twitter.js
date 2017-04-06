4fd254c7efb23e4b4e911c341c17e6f82443ebfc
jackzheng2496 <jackzheng2496@gmail.com>
2017-04-06 03:24:06 -0400

    id

--- twitter.js
+++ twitter.js
@@ -221,82 +221,84 @@ app.post("/additem", function (req, res) {
 		if (doc) {
             var id = (Math.random() + 1).toString(36).substring(15);
 
-            db.collection("users").findAndModify(
-                { username: sessionkey, verified: "yes" },
-                [],
-                { $push: { "tweets": {
-                              "id": id,   
-                              "username": sessionkey,
-                              "content": content,
-                              "timestamp": timestamp
-                        }} },
-               { new: true},
-                {w: 1}
-            , (error, result) => {
+            var documentA = new Map();
+            documentA.set("id", id);
+            documentA.set("username", sessionkey);
+            documentA.set("content", content);
+            documentA.set("timestamp", timestamp);
+            // {
+            //     "id": id,   
+            //     "username": sessionkey,
+            //     "content": content,
+            //     "timestamp": timestamp
+            // };
+                        
+
+            db.collection("tweets").insert({"id":documentA.get("id"),"username":documentA.get("username"),
+                "content":documentA.get("content"),"timestamp":documentA.get("timestamp")}, {w:1}, (err, result) => {
                 if (error) {
-                        console.error(new Error("ERROR INSERTING TWEET TO", req.cookies.key));
-                        console.log(error);
-                        res.json({status: "ERROR" });
-                    } else {
-                        // console.log(result);
-                        var tweets = result.tweets;
-                        console.log(tweets);
-
-                        var documentA = {
-                            "id": result.tweets[result.tweets.length].id,   
-                            "username": sessionkey,
-                            "content": content,
-                            "timestamp": timestamp
-                        };
-                        
-                        db.collection("tweets").insert(documentA, {w: 1}, (error, result) => {
-                                if (error) {
-                                    console.error(new Error("ERROR INSERTING TWEET TO DB"));
-                                    res.json({status: "ERROR"});
-                                } else { 
-                                    console.log("SUCCESS INSERTING", id);
-                                    res.json({status: "OK", id: id});
-                                }
-                        });
-                    }
+                    console.error(new Error("ERROR INSERTING TWEET TO DB"));
+                    res.json({status: "ERROR"});
+                } else {
+                    db.collection("users").update({username: sessionkey, verified: "yes"},
+                    {
+                        $push: {
+                            "tweets": {
+                                "id": documentA.get("id"),
+                                "username": sessionkey,
+                                "content": documentA.get("content"),
+                                "timestamp": documentA.get("timestamp")
+                            }
+                        }
+                    }, {w:1}, (err, result) => {
+                        if (err) {
+                             console.error(new Error("ERROR INSERTING TWEET TO DB"));
+                            res.json({status: "ERROR"});
+                        } else {
+                                console.log("SUCCESS INSERTING", id);
+                                res.json({status: "OK", id: documentA.get("id")});
+                        }
+                    });
+                }
             });
+            // db.collection("users").update({username: sessionkey, verified: "yes"},
+            //     {
+            //       $push: {
+            //             "tweets": {
+            //                   "id": id,   
+            //                   "username": sessionkey,
+            //                   "content": content,
+            //                   "timestamp": timestamp
+            //             }
+            //         } 
+            //     }, {w:1},(error, result) => {
+            //     if (error) {
+            //             console.error(new Error("ERROR INSERTING TWEET TO", req.cookies.key));
+            //             console.log(error);
+            //             res.json({status: "ERROR" });
+            //         } else {
+            //             // console.log(result);
+            //             var tweets = result.tweets;
+            //             console.log(tweets);
 
-
-         //    db.collection("users").findAndModify({
-         //        query: {username: sessionkey, verified: "yes"},
-         //        update: { $push: {
-         //                "tweets": {
-         //                      "id": id,   
-         //                      "username": sessionkey,
-         //                      "content": content,
-         //                      "timestamp": timestamp
-         //                }
-         //            } 
-         //        }, new: true, w:1}, (error, result) => {
-         //            if (error) {
-         //                console.error(new Error("ERROR INSERTING TWEET TO", req.cookies.key));
-         //                console.log(error);
-         //                res.json({status: "ERROR" });
-         //            } else {
-         //                console.log(result);
-         //                var documentA = {
-         //                    "id": result.tweets[result.tweets.length].id,   
-         //                    "username": sessionkey,
-         //                    "content": content,
-         //                    "timestamp": timestamp
-         //                };
+            //             var documentA = {
+            //                 "id": id,   
+            //                 "username": sessionkey,
+            //                 "content": content,
+            //                 "timestamp": timestamp
+            //             };
                         
-         //                db.collection("tweets").insert(documentA, {w: 1}, (error, result) => {
-    					// 		if (error) {
-    					// 			console.error(new Error("ERROR INSERTING TWEET TO DB"));
-         //                            res.json({status: "ERROR"});
-    					// 		} else { 
-         //                            console.log("SUCCESS INSERTING", id);
-    					// 			res.json({status: "OK", id: id});
-    					// 		}
-    					// });
-         //            }
-         //    });
+            //             db.collection("tweets").insert(documentA, {w: 1}, (error, result) => {
+            //                     if (error) {
+            //                         console.error(new Error("ERROR INSERTING TWEET TO DB"));
+            //                         res.json({status: "ERROR"});
+            //                     } else { 
+            //                         console.log("SUCCESS INSERTING", id);
+            //                         res.json({status: "OK", id: id});
+            //                     }
+            //             });
+            //         }
+            // });
         } else {
             res.json({status: "error", error: "USER IS NOT LOGGED IN"});
         }
