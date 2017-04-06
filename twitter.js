bb2287a1d6db61082545001cb267d693da658b4c
jackzheng2496 <jackzheng2496@gmail.com>
2017-04-05 23:18:19 -0400

    updating get item

--- twitter.js
+++ twitter.js
@@ -22,7 +22,7 @@ app.set("trust proxy", 1); //Trust first proxy
 
 
 
-MongoClient.connect("mongodb://130.245.168.187:27017/twitter", (err,database) => {
+MongoClient.connect("mongodb://localhost:27017/twitter", (err,database) => {
 //MongoClient.connect("mongodb://130.245.168.183:27017/twitter?replicaSet=twitter&readPreference=primary",function(error,database) {
 //MongoClient.connect("mongodb://130.245.168.251:27017,130.245.168.182:27017,130.245.168.183:27017,130.245.168.185:27017,130.245.168.187:27017/twitter?replicaSet=twitter&readPreference=primary", function (error, database) {
 //MongoClient.connect("mongodb://localhost:27017/twitter", function (error, database) {
